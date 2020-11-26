5154ff91c5c570cc5aa34f3e20617517bf03ff68
Oussama Aroua <oussama.aroua@orange.com>
2020-11-26 12:25:50 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyProfile());
+}
+
+class MyProfile extends StatelessWidget {
+  const MyProfile({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: columns(),
+        ),
+      ),
+    );
+  }
+
+  Row rows() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        item(Colors.teal, "Cell 1"),
+        item(Colors.redAccent, "Cell 2"),
+        item(Colors.greenAccent, "Bobo"),
+        SizedBox(
+          height: double.infinity,
+        )
+      ],
+    );
+  }
+
+  Column columns() {
+    return Column(
+      // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        item(Colors.teal, "Cell 1"),
+        item(Colors.redAccent, "Cell 2"),
+        item(Colors.greenAccent, "Bobo"),
+        item(Colors.red, "Bobo"),
+        item(Colors.blue, "Bobo"),
+        item(Colors.amber, "Bobo"),
+        item(Colors.greenAccent, "Bobo"),
+        item(Colors.amberAccent, "Bobo"),
+        item(Colors.blueGrey, "Bobo"),
+      ],
+    );
+  }
+
+  item(Color color, String text) {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.all(8),
+        margin: EdgeInsets.all(16),
+        height: 100,
+        width: double.infinity,
+        color: color,
+        child: Center(child: Text(text)),
+      ),
+    );
+  }
+}
